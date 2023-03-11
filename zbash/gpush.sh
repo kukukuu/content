89d0bb781bb591a61b1fb3ae62a5e45a67acd7b9
zhangxin <zhangxin8069@qq.com>
2023-03-11 15:37:41 +0800

    2023-03-11-15-37-41

--- zbash/gpush.sh
+++ zbash/gpush.sh
@@ -1,2 +1,1 @@
-git push --set-upstream origin zhangxin-lenovo-xiaoxinair-15alc-2021
-cd ~/content && git add * && git commit -m $(date "+%Y-%m-%d-%H-%M-%S") && git push && echo 'content have git_push successful'、
+cd ~/content && git push --set-upstream origin zhangxin-lenovo-xiaoxinair-15alc-2021 && git add * && git commit -m $(date "+%Y-%m-%d-%H-%M-%S") && git push && echo 'content have git_push successful'、
