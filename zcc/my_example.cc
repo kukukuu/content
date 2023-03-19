26971c754452b642022b8eebfebe8883c3478847
zhangxin <zhangxin8069@qq.com>
2023-03-19 17:58:06 +0800

    2023-03-19-17-58-06

--- zcc/my_example.cc
+++ zcc/my_example.cc
@@ -1,29 +1,30 @@
-#include "my_bistabcg.h"
+#include <iostream>
+#include "my_vector.h"
 int main()
 {
     MPI_Init(NULL, NULL);
-    int lattice_x(60), lattice_t(60), lattice_spin(2); // const lattice_spin=2
+    int lattice_x(3 * 4 * 500), lattice_t(3 * 4 * 5), lattice_spin(2); // const lattice_spin=2
     int size(lattice_x * lattice_t * lattice_spin);
     int MAX_ITER(1e3);
     double TOL(1e-5), start, end;
     ComplexVector b(size);
     ComplexVector U(size);
-    // b.clean_1();
-    // // b[0] = 10;
-    // U.clean_1();
+    b.clean_1();
+    b[0] = 10;
+    U.clean_1();
     b.clean_rand();
     U.clean_rand();
-    double mass(1);
-    bool dag(true);
+    std::complex<double> mass(1);
+    mass = b.dotX(U);
     start = MPI_Wtime();
-    BistabCG bicgstaCG(U, b, lattice_x, lattice_t, lattice_spin, mass, dag, MAX_ITER, TOL);
-    bicgstaCG.solve();
     end = MPI_Wtime();
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
     std::cout
         << "########"
         << "time cost:"
         << end - start
         << "s"
+        << mass / double(size)
         << std::endl;
     MPI_Finalize();
 
---
+++ zh/zbicgstab.h
@@ -0,0 +1,83 @@
+#ifndef _ZBICGSTAB_H
+#define _ZBICGSTAB_H
+#include "./zvector.h"
+#include "./zdslash.h"
+class zbicgstab
+{
+public:
+    zbicgstab(int MAX_ITER, double &TOL, zvector &b, zdslash &Zdslash) : MAX_ITER(MAX_ITER), TOL(TOL), Zdslash(Zdslash), dimensions(b.dimensions)
+    {
+        this->b = &b;
+    }
+
+    ~zbicgstab()
+    {
+    }
+
+    // solve the linear system Ax = b
+    void solve()
+    {
+        std::complex<double> rho_prev(1.0, 0.0);
+        std::complex<double> rho(0.0, 0.0);
+        std::complex<double> alpha(1.0, 0.0);
+        std::complex<double> omega(1.0, 0.0);
+        std::complex<double> beta(0.0, 0.0);
+        zvector x(dimensions);
+        zvector r = *b;
+        zvector r_tilde(dimensions);
+        zvector p(dimensions);
+        zvector v(dimensions);
+        zvector s(dimensions);
+        zvector t(dimensions);
+        // x.rand(); // initial guess
+        // Zdslash.dslash(x, r_tilde);
+        // // zvector r = b - A * x;
+        // r = r - r_tilde;
+        x.assign_zero(); // if x=0;r_tilde=r0=b;
+        r_tilde = r;
+        p.assign_zero();
+        v.assign_zero();
+        s.assign_zero();
+        t.assign_zero();
+        for (int i = 0; i < MAX_ITER; i++)
+        {
+            std::cout << "#"
+                      << i
+                      << "-Residual: "
+                      << r.norm2()
+                      << std::endl;
+            rho = r_tilde.dot(r);
+            beta = (rho / rho_prev) * (alpha / omega);
+            p = r + (p - v * omega) * beta;
+            // v = A * p;
+            Zdslash.dslash(p, v);
+            alpha = rho / r_tilde.dot(v);
+            s = r - v * alpha;
+            // t = A * s;
+            Zdslash.dslash(s, t);
+            omega = t.dot(s) / t.dot(t);
+            x = x + p * alpha + s * omega;
+            r = s - t * omega;
+            if (r.norm2() < TOL)
+            {
+                std::cout << "#End-loop: "
+                          << i
+                          << std::endl;
+                break;
+            }
+            rho_prev = rho;
+        }
+        std::cout << "#End-Residual: "
+                  << r.norm2()
+                  << std::endl;
+        x.display();
+    }
+
+private:
+    int MAX_ITER;
+    double TOL;
+    zvector *b;
+    zdslash Zdslash;
+    std::vector<int> dimensions;
+};
+#endif
